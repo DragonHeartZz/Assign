a3d785f71a0403aa2ba8e85d9c7017ae9d20a799
DragonHeart <DragonHeart>
2016-11-08 22:32:12 +0700

    fix #3 the ball didnt start form center after player 2 scored

--- vn/vanlanguni/ponggame/PongPanel.java
+++ vn/vanlanguni/ponggame/PongPanel.java
@@ -158,8 +158,8 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 						playing = false;
 						gameOver = true;
 					}
-					ballX = 200;
-					ballY = 200;
+					ballX = 240;
+					ballY = 240;
 				} else {
 					// If the ball hitting the paddle, it will bounce back
 					// FIXME Something wrong here
